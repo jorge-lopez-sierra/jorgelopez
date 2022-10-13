c2672745c8a1b40b0bcfd79d9b83611240cddf21
jorge-lopez-sierra <48024029+jorge-lopez-sierra@users.noreply.github.com>
2022-10-13 07:57:55 -0500

    Update appFlask.py

--- appFlask.py
+++ appFlask.py
@@ -7,19 +7,19 @@ app=Flask(_name_)
 
 @app.route(´/´)
 def index():
-    return render_template(´index.html´)
+    return render_template('index.html')
 
-@app.route(´/prediccion´,methods=[´GET´,´POST´])
+@app.route('/prediccion',methods=['GET','POST'])
 def predict():
-    if request.method==´POST´:
+    if request.method=='POST':
         try:
-            var_1=float(request.form[´var_1´])
-            var_2=float(request.form[´var_2´])
+            var_1=float(request.form['var_1'])
+            var_2=float(request.form['var_2'])
 
             pred_args=[var_1,var_2]
             pred_arr=np.array(pred_args)
             preds=pred_arr.reshape(1,-1)
-            modelo=open(´./Modelo.pkl´,´rb´)
+            modelo=open(´./Modelo.pkl','rb')
             prediccion_modelo=modelo_clas.predict(preds)
             prediccion_modelo=round(float(prediccion_modelo),2)
             if prediccion_modelo == 1.0:
@@ -28,6 +28,6 @@ def predict():
                 prediccion_modelo = "No aprueba"
             except ValueError:
                 return "Por favor entra nombre validos"
-            return render_template(´prediccion.html´,prediccion=prediccion_modelo)
-if _name_==´-main-´:
+            return render_template('prediccion.html',prediccion=prediccion_modelo)
+if _name_=='__main__':
     app.run(debug=True)
